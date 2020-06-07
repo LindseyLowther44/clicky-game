0369863797dc7cd270bdda44cf0298bfffa1a993
LindseyLowther44 <lindseymlowther@gmail.com>
2020-06-06 23:08:03 -0500

    first commit

---
+++ build/precache-manifest.e8abbc7c883212edd6865ec24600e062.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1529efedb36c4529851378bb48bad5dc",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "3e3cc0f1126836deb5d9",
+    "url": "/clicky-game/static/css/main.744620e4.chunk.css"
+  },
+  {
+    "revision": "55ea0a1c41e3cf21f12c",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.378b37ea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e3cc0f1126836deb5d9",
+    "url": "/clicky-game/static/js/main.eff03777.chunk.js"
+  },
+  {
+    "revision": "ddea091f236f02df6e9c",
+    "url": "/clicky-game/static/js/runtime-main.de0ba425.js"
+  }
+]);
